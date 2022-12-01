df88c28b1527416e34e938168017f6e1d65fb196
Grodaaa <engstrom.kristinaa@gmail.com>
2022-12-01 17:19:47 -0300

    Day 01

--- 2022/src/day01/day01.ts
+++ 2022/src/day01/day01.ts
@@ -2,13 +2,34 @@ import { Task } from "../task";
 
 export class Day01 extends Task {
     partOne(): String {
-        const input = this.readInput();
-        
-        console.log(input);
-        return "partone";
+        let elfs = this.getElfs();
+
+        return `${elfs[0]}`;
     }
     partTwo(): String {
+        let elfs = this.getElfs();
+
+        const result = elfs[0] + elfs[1] + elfs[2];
+
+        return `${result}`;
+    }
+    getElfs(): number[] {
+        const input = this.readInput();
+        let calorieList = input.split('\n');
+
+        let elfs : number[] = [];
         
-        return "parttwo";
+        let sumCalories = 0;
+        calorieList.forEach(element => {
+            if(Number.isNaN(parseInt(element))) {
+                elfs.push(sumCalories);
+                sumCalories = 0;
+            } else {
+                sumCalories += parseInt(element);
+            }
+        });
+
+        elfs.sort((n1,n2) => n2 - n1);
+        return elfs;
     }
 }
