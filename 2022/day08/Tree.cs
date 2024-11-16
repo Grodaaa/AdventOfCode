cc68a6c56b2302022e8ae9d83c4b0b2d41682ecd
grodaaa <engstrom.kristinaa@gmail.com>
2024-11-16 12:20:04 -0300

    Remove tree.cs

--- 2022/day08/Tree.cs
+++
@@ -1,9 +0,0 @@
-namespace AdventOfCode.Day08
-{
-    class Tree
-    {
-        public int Latitude { get; set; }
-        public int Longitude { get; set; }
-        public double Height { get; set; }
-    }
-}
