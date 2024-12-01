2e5b64acb5656b8dd0519838844180dbe204bcd5
Kristina <engstrom.kristinaa@gmail.com>
2024-12-01 21:12:18 +0100

    Day 1 2024

---
+++ 2024/DailyTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace AdventOfCode
+{
+    abstract class DailyTask
+    {
+        protected string Input { get; }
+
+        protected DailyTask()
+        {
+            Input = LoadInput();
+        }
+
+        public abstract string PartOne();
+        public abstract string PartTwo();
+
+        private string LoadInput()
+        {
+            var info = Assembly.GetExecutingAssembly().GetName();
+            var name = info.Name;
+            //var inputFileName = $"_{name}.inputs.{GetType().Name.ToLower()}_input.txt";
+            var inputFileName = $"AdventOfCode._2024.inputs.{GetType().Name.ToLower()}_input.txt";
+
+            var hej = Assembly
+            .GetExecutingAssembly().GetManifestResourceNames();
+
+            using var stream = Assembly
+                        .GetExecutingAssembly()
+                        .GetManifestResourceStream(inputFileName)!;
+
+            using var streamReader = new StreamReader(stream, Encoding.UTF8);
+
+            return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ 2024/day01/Day01.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.day01
+{
+    internal class Day01 : DailyTask
+    {
+        public override string PartOne()
+        {
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+
+            var diffs = new List<int>();
+            var splittedInput = Input.Split(Environment.NewLine);
+            foreach (var input in splittedInput)
+            {
+                var items = input.Split("   ");
+                firstList.Add(int.Parse(items[0]));
+                secondList.Add(int.Parse(items[1]));
+            }
+            firstList.Sort();
+            secondList.Sort();
+
+            foreach (var nw in firstList.Zip(secondList, Tuple.Create))
+            {
+                diffs.Add(Math.Abs(nw.Item1 - nw.Item2));
+            }
+
+            return diffs.Sum().ToString();
+        }
+
+        public override string PartTwo()
+        {
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+
+            var nums = new List<int>();
+
+            var diffs = new List<int>();
+            var splittedInput = Input.Split(Environment.NewLine);
+            foreach (var input in splittedInput)
+            {
+                var items = input.Split("   ");
+                firstList.Add(int.Parse(items[0]));
+                secondList.Add(int.Parse(items[1]));
+            }
+
+            foreach (var item in firstList)
+            {
+                var hits = secondList.FindAll(x => x == item);
+                nums.Add(item * hits.Count);
+            }
+
+            return nums.Sum().ToString();
+        }
+    }
+}
