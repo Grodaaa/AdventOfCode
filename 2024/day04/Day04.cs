1103e69844e52f967f96a718dea29df9cfebde41
Kristina <engstrom.kristinaa@gmail.com>
2024-12-04 23:28:58 +0100

    Finish day 4

--- 2024/day04/Day04.cs
+++ 2024/day04/Day04.cs
@@ -5,17 +5,22 @@
         private char[] _xmas = ['X', 'M', 'A', 'S'];
         public override string PartOne()
         {
-
             var numOfXmas = 0;
             var grid = GetGrid();
+            var count = 1;
 
             foreach (var item in grid)
             {
                 if (item.Letter == _xmas[0])
                 {
-                    var hitRight = SearchRight(item, grid);
-                    if (hitRight) { numOfXmas++; }
-
+                    if (SearchXmas(item, grid, 1, 0, count)) { numOfXmas++; }//Search right
+                    if (SearchXmas(item, grid, 1, -1, count)) { numOfXmas++; } //Search right, down
+                    if (SearchXmas(item, grid, 0, -1, count)) { numOfXmas++; } //Search down
+                    if (SearchXmas(item, grid, -1, -1, count)) { numOfXmas++; } //Search down, left
+                    if (SearchXmas(item, grid, -1, 0, count)) { numOfXmas++; } //Search left
+                    if (SearchXmas(item, grid, -1, 1, count)) { numOfXmas++; } //Search left, up
+                    if (SearchXmas(item, grid, 0, 1, count)) { numOfXmas++; } //Search up
+                    if (SearchXmas(item, grid, 1, 1, count)) { numOfXmas++; } //Search up, right
                 }
             }
 
@@ -24,35 +29,65 @@
 
         public override string PartTwo()
         {
-            throw new NotImplementedException();
-        }
+            var numOfMas = 0;
+            var grid = GetGrid();
 
-        private bool SearchRight(Coordinate item, List<Coordinate> grid)
-        {
-            var xCoord = item.X + 1;
-            var xmasPos = 1;
-
-            var wordFound = false;
-            while (true)
+            foreach (var item in grid)
             {
-                var hit = grid.FirstOrDefault(x => x.X == xCoord && x.Y == item.Y && x.Letter == _xmas[xmasPos]);
-                if (hit != null && xmasPos < _xmas.Length - 1)
+                if (item.Letter == 'A')
                 {
-                    xCoord++;
-                    xmasPos++;
-                }
-                else if (hit == null)
-                {
-                    break;
-                }
-                else if (hit != null && xmasPos == _xmas.Length - 1)
-                {
-                    wordFound = true;
-                    break;
+                    if (SearchMas(item, grid)) { numOfMas++; }
                 }
             }
 
+            return numOfMas.ToString();
+        }
+
+        private bool SearchXmas(Coordinate item, List<Coordinate> grid, int xDir, int yDir, int count)
+        {
+            var xCoord = item.X + xDir;
+            var yCoord = item.Y + yDir;
+
+            var wordFound = true;
+            while (count < _xmas.Length)
+            {
+                var hit = grid.FirstOrDefault(coord => coord.X == xCoord && coord.Y == yCoord && coord.Letter == _xmas[count]);
+                if (hit != null)
+                {
+                    if (xDir < 0) { xCoord--; }
+                    else if (xDir > 0) { xCoord++; }
+
+                    if (yDir < 0) { yCoord--; }
+                    if (yDir > 0) { yCoord++; }
+                }
+                else if (hit == null)
+                {
+                    wordFound = false;
+                    break;
+                }
+
+                count++;
+            }
+
             return wordFound;
+        }
+
+        private static bool SearchMas(Coordinate item, List<Coordinate> grid)
+        {
+            var mUpperRight = grid.Exists(coord => coord.X == item.X + 1 && coord.Y == item.Y - 1 && coord.Letter == 'M');
+            var mLowerRight = grid.Exists(coord => coord.X == item.X + 1 && coord.Y == item.Y + 1 && coord.Letter == 'M');
+            var mLowerLeft = grid.Exists(coord => coord.X == item.X - 1 && coord.Y == item.Y + 1 && coord.Letter == 'M');
+            var mUpperLeft = grid.Exists(coord => coord.X == item.X - 1 && coord.Y == item.Y - 1 && coord.Letter == 'M');
+
+            var sUpperRight = grid.Exists(coord => coord.X == item.X + 1 && coord.Y == item.Y - 1 && coord.Letter == 'S');
+            var sLowerRight = grid.Exists(coord => coord.X == item.X + 1 && coord.Y == item.Y + 1 && coord.Letter == 'S');
+            var sLowerLeft = grid.Exists(coord => coord.X == item.X - 1 && coord.Y == item.Y + 1 && coord.Letter == 'S');
+            var sUpperLeft = grid.Exists(coord => coord.X == item.X - 1 && coord.Y == item.Y - 1 && coord.Letter == 'S');
+
+            var axis1 = (mUpperRight && sLowerLeft) || (mLowerLeft && sUpperRight);
+            var axis2 = (mUpperLeft && sLowerRight) || (mLowerRight && sUpperLeft);
+
+            return axis1 && axis2;
         }
 
         private List<Coordinate> GetGrid()
